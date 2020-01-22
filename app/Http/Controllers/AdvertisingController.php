917688a98b52a5e5e005f10d1a68a591071be288
shokhrukh <shurikaxmedov1@gmail.com>
2020-01-22 12:57:30 +0500

    push

--- app/Http/Controllers/AdvertisingController.php
+++ app/Http/Controllers/AdvertisingController.php
@@ -11,11 +11,6 @@ class AdvertisingController extends Controller
     {
         $allAdvertising = Advertising::orderBy('id', 'DESC')->take(5)->get();
 
-        $allAdvertising->map(function ($advertising) {
-            $advertising->image = isset($advertising->image) ? secure_asset('uploads/advertising/' . $advertising->image) : null;
-            return $advertising;
-        });
-
         return response()->json(
             [
                 'result' => $allAdvertising
