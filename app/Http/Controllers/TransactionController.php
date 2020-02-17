084ddb6329bada855dceeb02f7121e2bf146a110
shokhrukh <aiiiypuk@mail.ru>
2020-02-17 16:58:07 +0500

    again fix

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -71,7 +71,7 @@ class TransactionController extends Controller
             return response()->json(
                 [
                     'error' => $response->error->message
-                ], 200);
+                ], 400);
         }
 
         
