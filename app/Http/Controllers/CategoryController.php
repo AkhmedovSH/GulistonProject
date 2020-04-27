d132a55da270ab8d696baa8029948cd777d6ce53
Answers <shurikaxmedov1@gmail.com>
2020-04-27 18:30:06 +0500

    category childrens added

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -19,9 +19,35 @@ class CategoryController extends Controller
             ], 200);
     }
 
-    public function getCategoryProducts($category_id)
+    public function getChildrensIds($categories, $category_id){
+        $category_ids = [];
+        foreach ($categories as $key => $value) {
+            # code...
+        }
+        
+    }
+
+    public function fetch_recursive($src_arr, $currentid, $parentfound = false, $cats = array())
     {
-        $allCategoryProducts = Product::where('category_id', $category_id)
+        $category_ids = [];
+        foreach($src_arr as $row)
+        {
+            if((!$parentfound && $row['id'] == $currentid) || $row['parent_id'] == $currentid)
+            {
+                array_push($category_ids, $row['id']);
+                if($row['parent_id'] == $currentid)
+                    $cats = array_merge($cats, $this->fetch_recursive($src_arr, $row['id'], true));
+            }
+        }
+        return $category_ids;
+    }
+
+    public function getCategoryProducts($category_id)
+    {   
+        $categories = Category::all();
+        $category_ids = $this->fetch_recursive($categories, $category_id);
+
+        $allCategoryProducts = Product::whereIn('category_id', $category_ids)
         ->paginate(20);
         
         return response()->json(
